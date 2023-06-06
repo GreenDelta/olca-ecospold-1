2955e46bab932490c570b4180dc0a4e9f9b8aa0b
msrocka <michael.srocka@gmail.com>
2023-06-06 20:23:13 +0200

    added Maven boilerplate + rw-test

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # olca-ecospold-1
 
-This is a Java library for reading and writing data sets in the EcoSpold 01 data
-format. It was originally part of the [openLCA modules](https://github.com/GreenDelta/olca-modules)
+This is a Java library for reading and writing data sets in the
+[EcoSpold 01 data format](https://ecoinvent.org/the-ecoinvent-database/data-formats/ecospold1/).
+It was originally part of the [openLCA modules](https://github.com/GreenDelta/olca-modules)
 but was moved to a separate repository since version 2.0.0.
 
 ## Usage
 
-...
+Add this dependency to your project:
+
+```xml
+<dependency>
+  <groupId>org.openlca</groupId>
+  <artifactId>olca-ecospold-1</artifactId>
+  <version>2.0.0</version>
+</dependency>
+```
+
+See the [read-write-test](./src/test/java/org/openlca/ecospold/WriteReadTest.java)
+for a simple example how data sets can be read and written.
--- src/test/java/org/openlca/ecospold/WriteReadTest.java
+++ src/test/java/org/openlca/ecospold/WriteReadTest.java
@@ -1,18 +1,27 @@
 package org.openlca.ecospold;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.openlca.ecospold.io.DataSet;
 import org.openlca.ecospold.io.DataSetType;
 import org.openlca.ecospold.io.EcoSpold;
 
-import java.io.File;
+import java.nio.file.Files;
 
 public class WriteReadTest {
 
 	@Test
-	public void testProcess() {
-		var target = new File("target/p.xml");
-		var factory = DataSetType.PROCESS.getFactory();
+	public void testProcess() throws Exception {
+		with(DataSetType.PROCESS.getFactory());
+	}
+
+	@Test
+	public void testImpactMethod() throws Exception {
+		with(DataSetType.IMPACT_METHOD.getFactory());
+	}
+
+	private void with(IEcoSpoldFactory factory) throws Exception {
 		var ds = factory.createDataSet();
 		var wrapper = new DataSet(ds, factory);
 		var ref = factory.createReferenceFunction();
@@ -20,9 +29,21 @@ public class WriteReadTest {
 		wrapper.setReferenceFunction(ref);
 		var spold = factory.createEcoSpold();
 		spold.getDataset().add(ds);
-		EcoSpold.write(target, spold);
+
+		var file = Files.createTempFile(
+			"_olca_test", ".xml").toFile();
+		EcoSpold.write(file, spold);
+
+		var name = EcoSpold.read(file)
+			.orElseThrow()
+			.getDataset()
+			.get(0)
+			.getMetaInformation()
+			.getProcessInformation()
+			.getReferenceFunction()
+			.getName();
+		assertEquals("a test", name);
+
+		Files.delete(file.toPath());
 	}
-
-
-
 }
